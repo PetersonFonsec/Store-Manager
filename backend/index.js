4c9cecfbf5ce43fe3a1a1d3afc8661b6653f77bb
Peterson Simião <petersonsimiao1@gmail.com>
2019-09-21 21:08:41 -0300

    add dotenv bcrypt and jwt-simple

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const server = require('./server/app')
 
 server.listen().then( ( {url} ) => console.log(`application running in ${url}`))
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,4 +1,17 @@
-// Update with your config settings.
+require('dotenv').config()
 
-const { knexParams } = require('./.env')
-module.exports = knexParams
+module.exports = {
+    client: process.env.DB,
+    connection: {
+        database: process.env.DB_NAME,
+        user:     process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+    migrations: {
+        tableName: 'knex_migrations'
+    }
+}
